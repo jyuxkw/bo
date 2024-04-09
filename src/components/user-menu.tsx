8737fcb6226b32b77a14f72256666066a92f23a9
jyuxkw <110702827+jyuxkw@users.noreply.github.com>
2024-04-09 08:45:44 +0800

    Update user-menu.tsx

--- src/components/user-menu.tsx
+++ src/components/user-menu.tsx
@@ -83,25 +83,11 @@ export function UserMenu() {
           <DropdownMenuSeparator />
           <DropdownMenuItem asChild>
             <a
-              href="https://github.com/weaigc/bingo/"
+              href=""
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex items-center justify-between w-full gap-2 cursor-pointer"
             >
-              开源地址
-              <IconGitHub />
-              <IconExternalLink className="w-3 h-3 ml-auto" />
-            </a>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <a
-              href="https://huggingface.co/login?next=%2Fspaces%2Fhf4all%2Fbingo%3Fduplicate%3Dtrue%26visibility%3Dpublic"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-flex items-center justify-between w-full gap-2 cursor-pointer"
-            >
-              复制站点
               <IconExternalLink className="w-3 h-3 ml-auto" />
             </a>
           </DropdownMenuItem>
